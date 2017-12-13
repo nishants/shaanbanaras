68527af80e325eadf468f590839c0600ecc92c51
nishant <nishant.singh87@gmail.com>
2017-12-13 13:45:05 +0530

    tope bar styling.

--- src/app/menu/food-menu-controller.js
+++ src/app/menu/food-menu-controller.js
@@ -6,6 +6,18 @@ app.controller("FoodMenuController", ["$scope", function($scope){
         image: {preview: 'assets/images/samples/food-1.jpg',},
         price : 180,
         title  : "Banarasi Chicken Shorba",
+      },
+      {
+        id    : 2,
+        image: {preview: 'assets/images/samples/food-1.jpg',},
+        price : 280,
+        title  : "Banarasi Chicken Shorba",
+      },
+      {
+        id    : 3,
+        image: {preview: 'assets/images/samples/food-1.jpg',},
+        price : 80,
+        title  : "Banarasi Chicken Shorba",
       }
     ]
   }
